f57d34aad4b299620dfeb86c1a478c192584b030
Vincent Coetzee <vincent.coetzee@icloud.com>
2021-08-20 16:55:45 +0200

    Initial Commit

---
+++ ArgonWorks/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  ArgonWorks
+//
+//  Created by Vincent Coetzee on 20/8/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ ArgonWorks/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ArgonWorks
+//
+//  Created by Vincent Coetzee on 20/8/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
